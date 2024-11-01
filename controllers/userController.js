c3c688dced0dca54be85a17bf2b063142be9fa22
fowosere ademola <fowosereademola@gmail.com>
2024-11-01 10:19:31 +0000

    Created a new userService file for the seperation of concerns

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,19 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const {
+  getUserList,
+  checkUser,
+  createUser,
+  removeUser,
+  modifyUser,
+} = require("../services/userService");
 
 const listUsers = async (req, res) => {
   try {
-    const users = await User.find();
+    const users = await getUserList();
     res.status(200).send(users);
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send({ message: "Unable to list users" });
   }
 };
 
@@ -14,52 +21,48 @@ const addUser = asyncHandler(async (req, res) => {
   const { name, email } = req.body;
 
   if (!name || !email) {
-    res.status(400);
-    throw new Error("Please include all fields");
+    res.status(400).send({ message: "Please input name and email!" });
   }
 
-  const userExists = await User.findOne({ email });
+  const userExists = await checkUser(email);
 
   if (userExists) {
-    res.status(400);
-    throw new Error("User already exists");
+    res.status(400).send({ message: "user already exists" });
   }
 
-  const user = await User.create({
-    name,
-    email,
-  });
+  const userCreation = await createUser(name, email);
 
-  if (user) {
+  if (userCreation) {
     res.status(201).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
+      _id: userCreation._id,
+      name: userCreation.name,
+      email: userCreation.email,
     });
   } else {
-    res.status(400);
-    throw new error("Invalid user data");
+    res.status(400).send({ message: "registration failed" });
   }
 });
 
 const deleteUser = async (req, res) => {
+  const userId = req.params.id;
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-
-    if (!user) return res.status(404).send({ message: "User not found" });
+    const userDeleted = await removeUser(userId);
+    if (!userDeleted)
+      return res.status(404).send({ message: "User not found" });
 
     res.status(200).send({ message: "User deleted succesfully!" });
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send({ message: "unable to delete user" });
   }
 };
 
 const updateUser = async (req, res) => {
+  const userId = req.params.id;
+  const userData = req.body;
+
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    res.status(201).send(user);
+    const userChanged = await modifyUser(userId, userData);
+    res.status(201).send({ message: "User updated successfully" });
   } catch (error) {
     res.status(404).send({
       message: "unable to insert data because data inserted not matched",
